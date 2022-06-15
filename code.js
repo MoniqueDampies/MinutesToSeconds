604174cb69afd60fb2703ff35352e3572a97f0d1
MoniqueDampies <moniquedampies1861@gmail.com>
2022-06-15 16:00:13 +0200

    first

---
+++ code.js
@@ -0,0 +1,8 @@
+
+function secConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("inputMinutes").value = valNum/60;}
+
+function minConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("inputSeconds").value = valNum*60;}
